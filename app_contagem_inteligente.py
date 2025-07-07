1c6bd270c5085be54d51ccd4861b358524ef284d
iparranca <isabel.parranca@gmail.com>
2025-07-08 00:53:22 +0100

    Update app_contagem_inteligente.py

--- app_contagem_inteligente.py
+++ app_contagem_inteligente.py
@@ -64,14 +64,14 @@ if uploaded_file:
         st.stop()
 
 
-primeira_coluna = df.columns[0]
+primeira_coluna1 = df.columns[0]
 
 # Verificar se há valores vazios
-if df[primeira_coluna].isnull().any():
+if df[primeira_coluna1].isnull().any():
     print("A primeira coluna tem valores vazios.")
 
 # Verificar se é uma coluna com datas
-convertida = pd.to_datetime(df[primeira_coluna], errors='coerce')
+convertida = pd.to_datetime(df[primeira_coluna1], errors='coerce')
 if convertida.notnull().sum() / len(df) > 0.8:
     print("A primeira coluna parece conter datas.")
     
