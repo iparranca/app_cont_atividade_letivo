1fb1716018c4db290eae9d5ff82a02c8820edf74
iparranca <isabel.parranca@gmail.com>
2025-07-08 19:36:13 +0100

    Update app_contagem_inteligente.py

--- app_contagem_inteligente.py
+++ app_contagem_inteligente.py
@@ -36,7 +36,7 @@ st.markdown("""
 st.info("""**Notas importantes**
 
 Ficheiro a carregar:  
-   a) Só pode carregar um ficheiro CSV (guarda o ficheiro Excel como CSV);  
+   a) Só pode carregar um ficheiro CSV (guarde o ficheiro Excel como CSV);  
    b) A primeira coluna (informação que está antes do primeiro separador, isto é, antes do primeiro ";" ou "," ou tabulação)  
       deve conter a informação da data ou Datahora. Esta informação irá referenciar o Ano Letivo;  
    c) Colocar na primeira linha uma linha nova com os cabeçalhos de cada coluna.  
